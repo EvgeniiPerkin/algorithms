bbfd6f4edd02438fddb21271b46e85000a492b2e
evgenii <evgeniiperkin@bk.ru>
2022-12-17 15:45:40 +0300

    First
    The first commit

---
+++ algorithms/src/com/perkin/dev/Learning.java
@@ -0,0 +1,16 @@
+package com.perkin.dev;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Learning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		StdOut.println("sdf");
+		StdOut.println("sdf");
+		StdOut.println("sdf");
+		StdOut.println("sdf");
+	}
+
+}

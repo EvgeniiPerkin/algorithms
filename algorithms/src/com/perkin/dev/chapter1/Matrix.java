d619a67509ebbf85c35473eeba4825ef669ad2d8
user <user@developer>
2023-01-17 15:27:34 +0300

    add file
    matrix

---
+++ algorithms/src/com/perkin/dev/chapter1/Matrix.java
@@ -0,0 +1,52 @@
+package com.perkin.dev.chapter1;
+
+public class Matrix {
+	/**
+	 * склярное произведение векторов
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public static double dot(double[] x, double[] y) {
+		return 0.0;
+	}
+	
+	/**
+	 * произведение двух матриц
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static double[][] mult(double[][] a, double[][] b) {
+		return null;
+	}
+	
+	/**
+	 * транспонирование
+	 * @param a
+	 * @return
+	 */
+	public static double[][] transpose(double[][] a) {
+		return null;
+	}
+	
+	/**
+	 * произведение матрицы на вектор
+	 * @param a
+	 * @param x
+	 * @return
+	 */
+	public static double[] mult(double[][] a, double[] x) {
+		return null;
+	}
+	
+	/**
+	 * произведение вектора на матрицу
+	 * @param y
+	 * @param a
+	 * @return
+	 */
+	public static double[] mult(double[] y, double[][] a) {
+		return null;
+	}
+}
